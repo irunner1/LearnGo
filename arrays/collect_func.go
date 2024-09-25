920adb69545f97f3d9b32c865e0ba44b2189c50d
irunner1 <aspuchkov02@gmail.com>
2024-09-25 10:00:02 +0300

    + modified arrays

---
+++ arrays/collect_func.go
@@ -0,0 +1,20 @@
+package arrays
+
+// Reduce accumulates collection by function
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
+	}
+	return result
+}
+
+// Find something in array by passed function
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
