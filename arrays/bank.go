77977217820f4efeff09667c74be9981541009d0
irunner1 <aspuchkov02@gmail.com>
2024-09-25 10:02:36 +0300

    arrays refactoring

--- arrays/bank.go
+++ arrays/bank.go
@@ -1,20 +1,24 @@
 package arrays
 
+// Transaction in Bank, passing sum from one accound to another
 type Transaction struct {
 	From string
 	To   string
 	Sum  float64
 }
 
+// NewTransaction creates transaction
 func NewTransaction(from, to Account, sum float64) Transaction {
 	return Transaction{From: from.Name, To: to.Name, Sum: sum}
 }
 
+// Account in Bank collects name and balance
 type Account struct {
 	Name    string
 	Balance float64
 }
 
+// NewBalanceFor creates transaction
 func NewBalanceFor(account Account, transactions []Transaction) Account {
 	return Reduce(
 		transactions,
@@ -23,6 +27,7 @@ func NewBalanceFor(account Account, transactions []Transaction) Account {
 	)
 }
 
+// applyTransaction applies transaction to both participants
 func applyTransaction(a Account, transaction Transaction) Account {
 	if transaction.From == a.Name {
 		a.Balance -= transaction.Sum
