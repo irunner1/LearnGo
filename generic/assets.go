4b2f3ac961c0834af4bd218b7806b6589a3fc6fc
irunner1 <aspuchkov02@gmail.com>
2024-09-25 09:43:41 +0300

    + chapter

---
+++ generic/assets.go
@@ -0,0 +1,35 @@
+package generics
+
+import "testing"
+
+// AssertEqual tests equal
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+// AssertNotEqual tests not equal
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %v", got)
+	}
+}
+
+// AssertTrue tests true value
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+// AssertFalse tests false value
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
